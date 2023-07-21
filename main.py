5df097cb18a57f1d6284800e6524fa606cc3a18e
Robert van Barlingen <robert.van.barlingen@outlook.com>
2023-07-21 18:53:05 +0200

    blah

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from io import BytesIO
 import base64
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/*": {"origins": [r"localhost:(\d+)", r"(\w+)\.edgeofdusk\.com", "edgeofdusk.com"]}})
 
 def get_file_name_without_extension(file_path):
     base_name = os.path.basename(file_path)
